19e30a34c6277d63ed83d114b57518d6418a9d67
Patty <L224749391@gmail.com>
2021-04-14 14:36:32 +0800

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by 林湘羚 on 2021/4/14.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
